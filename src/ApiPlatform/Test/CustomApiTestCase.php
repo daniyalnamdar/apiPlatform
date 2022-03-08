9d71c7875936e0315a1b321ab0fccacefd1b5590
Daniyal <bkmobil@Daniyal-MacBook-Pro.local>
2022-03-08 10:32:50 +0300

    chapter 14 done

--- src/ApiPlatform/Test/CustomApiTestCase.php
+++ src/ApiPlatform/Test/CustomApiTestCase.php
@@ -1,42 +1,50 @@
 <?php
 
-namespace App\Test;
+namespace App\ApiPlatform\Test;
 
-use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
-use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\Client;
+use App\ApiPlatform\Test\ApiTestCase;
+use App\ApiPlatform\Test\Client;
 use App\Entity\User;
 
 
-class CustomApiTestCase extends ApiTestCase
+class CustomApiTestCase extends \ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase
 {
+
+
     protected function createUser(string $email, string $password): User
     {
         $user = new User();
         $user->setEmail($email);
         $user->setUsername(substr($email, 0, strpos($email, '@')));
-        $encoded = self::$container->get('security.password_encoder')
-            ->encodePassword($user, $password);
-        $user->setPassword($encoded);
+
+//        $encoded = self::$container->get('security.password_encoder')->encodePassword($user, $password);
+        $user->setPassword($password);
+
         $em = self::$container->get('doctrine')->getManager();
         $em->persist($user);
         $em->flush();
+
         return $user;
     }
-    protected function logIn(Client $client, string $email, string $password)
+
+    protected function logIn(\ApiPlatform\Core\Bridge\Symfony\Bundle\Test\Client $client, string $email, string $password)
     {
         $client->request('POST', '/login', [
             'headers' => ['Content-Type' => 'application/json'],
             'json' => [
                 'email' => $email,
-                'password' => $password
+                'password' => 'tada'
             ],
         ]);
         $this->assertResponseStatusCodeSame(204);
     }
-    protected function createUserAndLogIn(Client $client, string $email, string $password): User
+
+    protected function createUserAndLogIn(\ApiPlatform\Core\Bridge\Symfony\Bundle\Test\Client $client, string $email, string $password): User
     {
         $user = $this->createUser($email, $password);
-        $this->logIn($client, $email, $password);
+
+        $this->logIn($client, $email, 'tada');
+
         return $user;
     }
 }
--- tests/functional/CheeseListingResourceTest.php
+++ tests/functional/CheeseListingResourceTest.php
@@ -3,23 +3,39 @@
 namespace App\Tests\functional;
 
 
-use App\Test\CustomApiTestCase;
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\User;
 use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
 
 
-class CheeseListingResourceTest extends CustomApiTestCase
-
+class CheeseListingResourceTest extends ApiTestCase
 {
     use ReloadDatabaseTrait;
     public function testCreateCheeseListing()
     {
         $client = self::createClient();
+//        $client->request('POST', '/api/cheeses', [
+//            'headers' => ['Content-Type' => 'application/json']
+//        ]);
         $client->request('POST', '/api/cheeses');
         $this->assertResponseStatusCodeSame(401);
 
-        $this->createUserAndLogIn($client,'cheese@gmail.com','123456');
+        $user = new User();
+        $user->setEmail('cheeseplease@example.com');
+        $user->setUsername('cheeseplease');
+        $user->setPassword('$2y$13$Da7nL4GBqavL3hwT5q5u4uOquDrZzTwgDCIT43TXdU3hwH3.I5HKW')
+        ;
+        $em = self::$container->get('doctrine')->getManager();
+        $em->persist($user);
+        $em->flush();
 
-        $client->request('POST', '/api/cheeses');
-        $this->assertResponseStatusCodeSame(400);
+        $client->request('POST', '/login', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'email' => 'cheeseplease@example.com',
+                'password' => 'foo'
+            ],
+        ]);
+        $this->assertResponseStatusCodeSame(204);
     }
 }
