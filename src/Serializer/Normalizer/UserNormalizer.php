05675bfddf7256f6b82d357703da208bfa51f782
Daniyal Namdar <daniyal.namdar@bktomorrow.com>
2022-08-19 15:29:31 +0300

    API PLATFORM PART2: Security chapter 29, 30 and 31

---
+++ src/Serializer/Normalizer/UserNormalizer.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Serializer\Normalizer;
+
+use App\Entity\User;
+use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
+use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class UserNormalizer implements ContextAwareNormalizerInterface, CacheableSupportsMethodInterface, NormalizerAwareInterface
+{
+    use NormalizerAwareTrait;
+
+    private const ALREADY_CALLED = 'USER_NORMALIZER_ALREADY_CALLED';
+    /**
+     * @param User $object
+     */
+    public function normalize($object, $format = null, array $context = array()): array
+    {
+        if ($this->userIsOwner($object)) {
+            $context['groups'][] = 'owner:read';
+        }
+
+        $context[self::ALREADY_CALLED] = true;
+        $data = $this->normalizer->normalize($object, $format, $context);
+
+        // Here: add, edit, or delete some data
+
+        return $data;
+    }
+
+    public function supportsNormalization($data, $format = null, array $context = []): bool
+    {
+        // avoid recursion: only call once per object
+        if (isset($context[self::ALREADY_CALLED])) {
+            return false;
+        }
+        return $data instanceof User;
+    }
+
+    private function userIsOwner(User $user): bool
+    {
+        return mt_rand(0, 10) > 5;
+    }
+
+    public function hasCacheableSupportsMethod(): bool
+    {
+        return false;
+    }
+}
